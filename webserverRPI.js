a1d09035eea42f1967befef65400518d690a5580
boggdan95 <bandabog@gmail.com>
2018-08-02 19:49:49 -0600

    training 2 - fixed 2nd intent *creating local function and variable

--- webserverRPI.js
+++ webserverRPI.js
@@ -4,22 +4,23 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var rn = require('random-number');
-var status = 0
+ var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var led1 = new Gpio(4, 'out'); //use GPIO pin 4 as output
-var led2 = new Gpio(17, 'out'); //use GPIO pin 17 as output
-var led3 = new Gpio(27, 'out'); //use GPIO pin 27 as output
-var led4 = new Gpio(22, 'out'); //use GPIO pin 22 as output
-var pushButton1 = new Gpio(18, 'in', 'falling', {debounceTimeout: 5}); //use GPIO pin 18 as input, and 'both' button presses, and releases should be handled
-var pushButton2 = new Gpio(23, 'in', 'falling', {debounceTimeout: 5}); //use GPIO pin 23 as input, and 'both' button presses, and releases should be handled
-var pushButton3 = new Gpio(24, 'in', 'both'); //use GPIO pin 24 as input, and 'both' button presses, and releases should be handled
-var pushButton4 = new Gpio(25, 'in', 'both'); //use GPIO pin 25 as input, and 'both' button presses, and releases should be handled
-var led = [led1,led2,led3,led4];
-var inputButtons = [pushButton1,pushButton2,pushButton3,pushButton4];
+ var led1 = new Gpio(4, 'out'); //use GPIO pin 4 as output
+ var led2 = new Gpio(17, 'out'); //use GPIO pin 17 as output
+ var led3 = new Gpio(27, 'out'); //use GPIO pin 27 as output
+ var led4 = new Gpio(22, 'out'); //use GPIO pin 22 as output
+ var pushButton1 = new Gpio(18, 'in', 'falling', {debounceTimeout: 5}); //use GPIO pin 18 as input, and 'both' button presses, and releases should be handled
+ var pushButton2 = new Gpio(23, 'in', 'falling', {debounceTimeout: 5}); //use GPIO pin 23 as input, and 'both' button presses, and releases should be handled
+ var pushButton3 = new Gpio(24, 'in', 'both'); //use GPIO pin 24 as input, and 'both' button presses, and releases should be handled
+ var pushButton4 = new Gpio(25, 'in', 'both'); //use GPIO pin 25 as input, and 'both' button presses, and releases should be handled
+ var led = [led1,led2,led3,led4];
+ var inputButtons = [pushButton1,pushButton2,pushButton3,pushButton4];
+ var status = 0;
 
-//training 2 - fail 2nd intent   // training 3 - fail alternate output
-
+ function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
+ }
 
 app.use(express.static(path.join(__dirname + '/public')));
 // viewed at http://localhost:8080
@@ -36,45 +37,71 @@ http.listen(3000, function(){
 //******************************************************************************
 //******************************************************************************
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function on(led_select){
-  status = 1;
-  if (led_select !== undefined){
-  if (status != led_select.readSync()) { //only change LED if status has changed
-         led_select.writeSync(status); //turn LED on or off
-    }
-  }
-  else {
-    if (status != led[0].readSync()) { //only change LED if status has changed
-           led[0].writeSync(status); //turn LED on or off
-      }
-  }
-  console.log('on');
-}
-
-function off(led_select){
-  status = 0;
-  if (led_select !== undefined){
-  if (status != led_select.readSync()) { //only change LED if status has changed
-         led_select.writeSync(status); //turn LED on or off
-    }
-  }
-  else {
-    if (status != led[0].readSync()) { //only change LED if status has changed
-           led[0].writeSync(status); //turn LED on or off
-      }
-  }
-  console.log('off');
-}
-
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   console.log('Conexion iniciada');
-  //****************************************************************************
-  //****************************************************************************
+//******************************************************************************
+//******************************************************************************
 
+  function changeState(led_select,value) {
+    if (led_select !== undefined){
+    if (value != led_select.readSync()) { //only change LED if status has changed
+           led_select.writeSync(value); //turn LED on or off
+      }
+    }
+    else {
+      if (value != led[0].readSync()) { //only change LED if status has changed
+             led[0].writeSync(value); //turn LED on or off
+        }
+    }
+    return value;
+  }
+
+  function on(led_select){
+    status = 1;
+    if (led_select !== undefined){
+    if (status != led_select.readSync()) { //only change LED if status has changed
+           led_select.writeSync(status); //turn LED on or off
+      }
+    }
+    else {
+      if (status != led[0].readSync()) { //only change LED if status has changed
+             led[0].writeSync(status); //turn LED on or off
+        }
+    }
+    console.log('on');
+  }
+
+  function off(led_select){
+    status = 0;
+    if (led_select !== undefined){
+    if (status != led_select.readSync()) { //only change LED if status has changed
+           led_select.writeSync(status); //turn LED on or off
+      }
+    }
+    else {
+      if (status != led[0].readSync()) { //only change LED if status has changed
+             led[0].writeSync(status); //turn LED on or off
+        }
+    }
+    console.log('off');
+  }
+
+  function onSetTimeout(time,modulo,i) {
+  setTimeout(function() {
+    on(led[modulo[i]]);
+    onTime = Date.now();
+  }, time[i]);
+}
+
+  function offSetTimeout(time,modulo,reaction,ans,a,i) {
+    a.push(setTimeout(function () {
+      off(led[modulo[i]]);
+      reaction.push('--');
+      ans.push('Miss');
+    },time[i]));
+}
+
+//******************************************************************************
   socket.on('entrenamiento1', function(data) {
     const totalTime = 90000; //Tiempo total del ejercicio
     const noTry = data; //Numero de repeticiones
@@ -130,6 +157,9 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
  });
 
     socket.on('entrenamiento2', function() {
+
+
+
       const step = 10;
       const stepTime = getRandomInt(3,5)*1000;
       const limitTime = 500;
@@ -137,15 +167,13 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
       var desactivationTime = [];
       var reactionTime = [];
       var hitmissArray = [];
+      var status = 0;
 
       for (var i = 1; i < step+1; i++) {
         activationTime.push(i*stepTime);
       }
       for (var i = 0; i < step; i++) {
         desactivationTime.push(activationTime[i]+limitTime);
-      }
-
-      for (var i = 0; i < activationTime.length; i++) {
         console.log(activationTime[i]);
         console.log(desactivationTime[i]);
       }
@@ -157,42 +185,42 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
       for (var i = 0; i < activationTime.length; i++) {
         setTimeout(function () {
           s = s + i;
-          console.log(s);
-          on(led[0]);
+          console.log("yyy", s);
+          status = changeState(led[0],1);
           onTime = Date.now();
         },activationTime[i]);
       }
      for (var i = 0; i < desactivationTime.length; i++) {
            timeOFF.push(setTimeout(function () {
-             off(led[0]);
+             status = changeState(led[0],0);
              reactionTime.push('--');
              hitmissArray.push('Miss');
            },desactivationTime[i]));
-         }
+     }
 
-         setTimeout(function () {
-           console.log('Entrenamiento terminado');
-           console.log(reactionTime);
-           console.log(hitmissArray);
-           s = 0;
-         },desactivationTime[desactivationTime.length-1]+2000);
+     setTimeout(function () {
+       console.log('Entrenamiento terminado');
+       console.log(reactionTime);
+       console.log(hitmissArray);
+       s = 0;
+     }, desactivationTime[desactivationTime.length-1]+2000);
 
-         inputButtons[0].watch(function (err, value) { //Watch for hardware interrupts on pushButton
-              if (err) {console.error('There was an error', err); //output error message to console
-              return err;
-            }
-            if (value != status) {
-              console.log(s);
-              console.log('Cambio de estado');
-              clearTimeout(timeOFF[(s/10)-1]);
-              off(led[0]);
-              console.log('TimeOFF length', timeOFF.length);
-              reaction = Date.now();
-              reactionTime.push(reaction-onTime);
-              hitmissArray.push('Hit');
-            }
-         });
+     inputButtons[0].watch(function (err, value) { //Watch for hardware interrupts on pushButton
+        if (err) {console.error('There was an error', err); //output error message to console
+          return err;
+        }
 
+        if (value != status) {
+          console.log("xxx", s);
+          console.log('Cambio de estado');
+          clearTimeout(timeOFF[(s/10)-1]);
+          status = changeState(led[0],0);
+          console.log('TimeOFF length', timeOFF.length);
+          reaction = Date.now();
+          reactionTime.push(reaction-onTime);
+          hitmissArray.push('Hit');
+        }
+     });
  });
 
 
@@ -211,9 +239,6 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
    }
    for (var i = 0; i < step; i++) {
      desactivationTime.push(activationTime[i]+limitTime);
-   }
-
-   for (var i = 0; i < step; i++) {
      activationModule.push(getRandomInt(0,2));
    }
 
@@ -228,19 +253,28 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
    var onTime;
    var reaction;
    for (var i = 0; i < activationTime.length; i++) {
-     setTimeout(function () {
-       s = s + i;
-       on(led[activationModule[i]]);
-       onTime = Date.now();
-     },activationTime[i]);
+     // setTimeout(function () {
+     s = i;
+     //   on(led[activationModule[i]]);
+     //   onTime = Date.now();
+     // },activationTime[i]);
+     onSetTimeout(activationTime,activationModule,i);
    }
   for (var i = 0; i < desactivationTime.length; i++) {
-        timeOFF.push(setTimeout(function () {
-          off(led[activationModule[i]]);
-          reactionTime.push('--');
-          hitmissArray.push('Miss');
-        },desactivationTime[i]));
+        // timeOFF.push(setTimeout(function () {
+        //   off(led[activationModule[i]]);
+        //   reactionTime.push('--');
+        //   hitmissArray.push('Miss');
+        // },desactivationTime[i]));
+    offSetTimeout(desactivationTime,activationModule,reactionTime,hitmissArray,timeOFF,i);
       }
+
+      setTimeout(function () {
+        console.log('Entrenamiento terminado');
+        console.log(reactionTime);
+        console.log(hitmissArray);
+        s = 0;
+      }, desactivationTime[desactivationTime.length-1]+2000);
 
       inputButtons[0].watch(function (err, value) { //Watch for hardware interrupts on pushButton
            if (err) {console.error('There was an error', err); //output error message to console
@@ -248,7 +282,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
          }
          if (value != status) {
            off(led[0]);
-           clearTimeout(timeOFF[(s/10)-1]);
+           clearTimeout(timeOFF[s]);
            reaction = Date.now();
            reactionTime.push(reaction-onTime);
            hitmissArray.push('Hit');
@@ -260,7 +294,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
          }
          if (value != status) {
            off(led[1]);
-           clearTimeout(timeOFF[(s/10)-1]);
+           clearTimeout(timeOFF[s]);
            reaction = Date.now();
            reactionTime.push(reaction-onTime);
            hitmissArray.push('Hit');
@@ -268,9 +302,6 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
       });
 
 });
-
-
-
 
 });
 
@@ -284,3 +315,5 @@ process.on('SIGINT', function () { //on ctrl+c
   process.exit(); //exit completely
 
   });
+
+// Funciones
